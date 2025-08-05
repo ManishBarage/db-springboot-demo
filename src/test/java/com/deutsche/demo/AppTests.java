6932bdddb318377c060dad143068d18ec5b271da
Mansi <baragemanish6258@gmail.com>
2025-08-05 12:25:19 +0530

    workflow done

---
+++ src/test/java/com/deutsche/demo/AppTests.java
@@ -0,0 +1,13 @@
+package com.deutsche.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTests {
+
+	@Test
+	void testTest() {
+		assertEquals(10, 5 + 5, "Wrong maths!");
+	}
+
+}
